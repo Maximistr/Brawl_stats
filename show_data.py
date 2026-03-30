2115e53eb76fd14775269c5e45d2b4298602be48
Maximistr <maximistr100@gmail.com>
2026-03-30 17:44:32 +0200

    fix: update show_data.py to set fixed width for visualizations in Streamlit

--- show_data.py
+++ show_data.py
@@ -29,9 +29,9 @@ tropy_line = px.line(tropy_data, x='Date', y='Trophies',
 
 with col1:
     st.write("### Distribution of Brawlers by Power Level") 
-    st.plotly_chart(power_bars, width=True)
+    st.plotly_chart(power_bars, width=400)
 
 with col2:
     st.write("### Trophies Over Time")
-    st.plotly_chart(tropy_line, width=True)
+    st.plotly_chart(tropy_line, width=400)
 
