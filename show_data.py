1bd4edaee9279a77f6c8e5a69899c320708c6ae7
Maximistr <maximistr100@gmail.com>
2026-03-30 17:40:15 +0200

    feat: update show_data.py to use Plotly for visualizations and enhance trophy data display

--- show_data.py
+++ show_data.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+import plotly.express as px
 import json
 import pandas as pd
 import numpy as np
@@ -7,19 +7,30 @@ import streamlit as st
 
 with open('player_data.json', 'r') as file:
     data = json.load(file)
-
+    
+col1, col2 = st.columns(2)
+    
 brawlers = pd.DataFrame(data["brawlers"])
 tropy_data = pd.read_csv('trophies.csv', names=['Date', 'Trophies'])
-
-figure, axes = plt.subplots(2,2, figsize=(12, 8))
-
 power_levels = brawlers['power'].value_counts()
-power_bars = axes[0,0].bar(power_levels.index, power_levels.values, color='purple', fc="black")
-axes[0,0].bar_label(power_bars)
-axes[0,0].set_title('Power Levels of Brawlers')
-axes[0,0].set_xlabel('Power Level')
 
-axes[0,1].plot(tropy_data['Date'], tropy_data['Trophies'], marker='o', color='yellow')
-axes[0,1].set_title('Trophy Progression')
-axes[0,1].set_ylabel('Trophies')
-st.pyplot(figure)
+power_bars = px.bar(power_levels, x=power_levels.index, y=power_levels.values,
+             labels={'x': 'Power Level', 'y': 'Number of Brawlers'},
+             title='Distribution of Brawlers by Power Level',
+             text=power_levels.values, template='plotly_dark',
+             color=power_levels.index, text_auto=False)
+power_bars.update_traces(textposition='outside')
+
+tropy_line = px.line(tropy_data, x='Date', y='Trophies',
+             labels={'Date': 'Date', 'Trophies': 'Trophies'},
+             title='Trophies Over Time',
+             template='plotly_dark')
+
+with col1:
+    st.write("### Distribution of Brawlers by Power Level") 
+    st.plotly_chart(power_bars, use_container_width=True)
+
+with col2:
+    st.write("### Trophies Over Time")
+    st.plotly_chart(tropy_line, use_container_width=True)
+
